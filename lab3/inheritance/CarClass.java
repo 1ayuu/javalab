7b10ab98068301163f1a9b88ac9a8a838b5ed109
1ayuu <46105942+1ayuu@users.noreply.github.com>
2020-02-11 18:17:04 +0545

    Update CarClass.java

--- lab3/inheritance/CarClass.java
+++ lab3/inheritance/CarClass.java
@@ -1,1 +1,17 @@
 
+class CarClass extends VehicleClass
+{
+	CarClass()
+	{
+		//super must be declared at first 
+		super("Small and compact", 4);
+	}
+}
+
+class BusClass extends VehicleClass
+{
+	BusClass()
+	{
+		super("Big and more seats", 4);
+	}
+}
